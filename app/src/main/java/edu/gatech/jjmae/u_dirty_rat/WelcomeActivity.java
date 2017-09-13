2918622fb8bc42de175e1d63746a1d8e45c6b07a
Madison Grams <mgrams3@gatech.edu>
2017-09-13 14:33:37 -0400

    adding initial project files

---
+++ app/src/main/java/edu/gatech/jjmae/u_dirty_rat/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.jjmae.u_dirty_rat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
