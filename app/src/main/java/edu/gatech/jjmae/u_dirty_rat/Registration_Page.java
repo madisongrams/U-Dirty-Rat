d855949e57f8aa6ace0c3157d4f3c599b6bab7d2
adyamodi <adyamodi@gatech.edu>
2017-10-02 00:06:13 -0400

    Added backend stuff for registration page-now password restrictions apply

--- app/src/main/java/edu/gatech/jjmae/u_dirty_rat/Registration_Page.java
+++ app/src/main/java/edu/gatech/jjmae/u_dirty_rat/Registration_Page.java
@@ -2,11 +2,15 @@ package edu.gatech.jjmae.u_dirty_rat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Button;
 import java.util.ArrayList;
 import java.util.List;
-import android.view.View.OnClickListener;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -15,21 +19,27 @@ import android.content.Intent;
 public class Registration_Page extends AppCompatActivity {
 
     private Spinner spinner;
+    private EditText newUsernameView;
+    private EditText newPasswordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration__page);
+        newUsernameView = (EditText) findViewById(R.id.editText);
+        newPasswordView = (EditText) findViewById(R.id.editText2);
         Button createaccount = (Button) findViewById(R.id.button);
         addItemsOnSpinner();
         addListenerOnSpinnerItemSelection();
         createaccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (properRegistration()) {
                     Intent myIntent = new Intent(view.getContext(), HomeActivity.class);
                     startActivityForResult(myIntent, 0);
-                setContentView(R.layout.activity_welcome);
+                    setContentView(R.layout.activity_welcome);
                 }
+            }
         });
 
     }
@@ -45,6 +55,64 @@ public class Registration_Page extends AppCompatActivity {
     }
     public void addListenerOnSpinnerItemSelection() {
         spinner = (Spinner) findViewById(R.id.spinner);
-       // spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+      //  spinner.setOnItemSelectedListener(new OnItemSelectedListener());
+    }
+    private boolean properRegistration() {
+
+        // Store values at the time of the login attempt.
+        String username = newUsernameView.getText().toString();
+        String password = newPasswordView.getText().toString();
+        if (username == null || password == null) {
+            return false;
+        }
+
+        // Check for a valid password
+        if (TextUtils.isEmpty(password)) {
+            displayErrorMessage(getApplicationContext().getString(R.string.error_field_required));
+            return false;
+        } else if (!isPasswordValid(password)) {
+            displayErrorMessage(getApplicationContext().getString(R.string.error_invalid_password));
+            return false;
+        }
+
+        // Check for a valid username.
+        if (TextUtils.isEmpty(username)) {
+            displayErrorMessage(getApplicationContext().getString(R.string.error_field_required));
+            return false;
+        } else if (!isUsernameValid(username)) {
+            displayErrorMessage(getApplicationContext().getString(R.string.error_invalid_email));
+            return false;
+        }
+        UserData.register(username, password,false);
+        return true;
+    }
+
+    private boolean isUsernameValid(String username) {
+        //TODO: decide what makes username valid?
+     //   if(username.length() < 5) {
+         //   return false;
+       // }
+
+        return true;
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: make this better and more clear
+        return password.length() > 2;
+    }
+
+    private void displayErrorMessage(String error) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        Log.e("error", "displayErrorMessage: " + error);
+        builder.setTitle("Error")
+                .setMessage(error)
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // co nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
+
