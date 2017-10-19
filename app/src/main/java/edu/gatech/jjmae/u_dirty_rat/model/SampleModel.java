046aba45dbbc92fd33e4e9edc0cec55146168641
Justin Paul <jpaul@lawn-143-215-123-92.lawn.gatech.edu>
2017-10-19 10:32:39 -0400

    model view controller layout refactor

---
+++ app/src/main/java/edu/gatech/jjmae/u_dirty_rat/model/SampleModel.java
@@ -0,0 +1,64 @@
+package edu.gatech.jjmae.u_dirty_rat.model;
+
+import android.util.Log;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Justin on 10/9/2017.
+ */
+
+/**
+ * model class to hold a list of rat data
+ */
+public class SampleModel {
+    public static final SampleModel INSTANCE = new SampleModel();
+
+    private List<RatSightingDataItem> items;
+    private int currentid = 40000000;
+
+    /**
+     * constructor that initializes backing array
+     */
+    private SampleModel() {
+        items = new ArrayList<>();
+    }
+
+    /**
+     * method to add item to backing array
+     * @param item item to be added to array
+     */
+    public void addItem(RatSightingDataItem item) {
+        items.add(item);
+    }
+
+    /**
+     * getter for backing array
+     * @return backing array
+     */
+
+    public List<RatSightingDataItem> getItems() {
+        return items;
+    }
+
+    /**
+     * search method to find items
+     * @param id id of item to be found
+     * @return item found or null if id not in list
+     */
+    public RatSightingDataItem findItemById(int id) {
+        for (RatSightingDataItem d : items) {
+            if (d.get_ID() == id) return d;
+        }
+        Log.d("MYAPP", "Warning - Failed to find id: " + id);
+        return null;
+    }
+
+    /**
+     * current id is the id to be used for a new rat
+     * @return current id
+     */
+    public int getCurrentid() {
+        return currentid++;
+    }
+}
