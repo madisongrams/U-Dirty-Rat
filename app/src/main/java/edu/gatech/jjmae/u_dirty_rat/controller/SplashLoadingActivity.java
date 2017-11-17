da4eb37469674fc7961bb09204b8002b9feee87e
Justin Paul <jpaul@lawn-143-215-112-251.lawn.gatech.edu>
2017-11-17 13:15:35 -0500

    new app startup background

--- app/src/main/java/edu/gatech/jjmae/u_dirty_rat/controller/SplashLoadingActivity.java
+++ app/src/main/java/edu/gatech/jjmae/u_dirty_rat/controller/SplashLoadingActivity.java
@@ -1,30 +1,14 @@
 package edu.gatech.jjmae.u_dirty_rat.controller;
 
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.drawable.Animatable2;
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.animation.Animation;
-import android.widget.ImageView;
-
-import java.io.File;
-import java.io.IOException;
-
-import edu.gatech.jjmae.u_dirty_rat.MyApp;
 import edu.gatech.jjmae.u_dirty_rat.R;
-import edu.gatech.jjmae.u_dirty_rat.model.SampleModel;
-import pl.droidsonroids.gif.AnimationListener;
-import pl.droidsonroids.gif.GifDrawable;
 
 public class SplashLoadingActivity extends AppCompatActivity {
 
-    private final int DISPLAY_TIME = 3500;
-//    private MyApp app;
+    private final int DISPLAY_TIME = 4200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,34 +36,3 @@ public class SplashLoadingActivity extends AppCompatActivity {
         super.onResume();
     }
 }
-
-
-
-//        GifDrawable gifDrawable = null;
-//        try {
-//            gifDrawable = new GifDrawable( getAssets(), "splash_logo_anim.gif" );
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        if (gifDrawable != null) {
-//            gifDrawable.addAnimationListener(new AnimationListener() {
-//                @Override
-//                public void onAnimationCompleted(int loopNumber) {
-//                    Log.d("splashscreen","Gif animation completed");
-//                    if (can_be_finished && nextIntent != null){
-//                        startActivity(nextIntent);
-//                        finish();
-//                    }else {
-//                        can_be_finished = true;
-//                    }
-//                }
-//            });
-//        }
-//        logoView.setImageDrawable(gifDrawable);
-//
-//        setContentView(R.layout.activity_splash_loading);
-//        Intent intent = new Intent(this, SplashLoadingActivity.class);
-//        startActivity(intent);
-//        finish();
-//    }
-//}
