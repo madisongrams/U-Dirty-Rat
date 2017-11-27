c73ba3ae588e65a3a1f4ab9461c26fd7b8ce9eaf
Justin Paul <jpaul@lawn-143-215-115-69.lawn.gatech.edu>
2017-11-27 13:32:11 -0500

    minor errors with merge

--- app/src/main/java/edu/gatech/jjmae/u_dirty_rat/controller/ExistingUserLoginActivity.java
+++ app/src/main/java/edu/gatech/jjmae/u_dirty_rat/controller/ExistingUserLoginActivity.java
@@ -40,9 +40,9 @@ public class ExistingUserLoginActivity extends AppCompatActivity {
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
-        View mLoginFormView = findViewById(R.id.login_form);
-        View mProgressView = findViewById(R.id.login_progress);
+//        mLoginFormView = findViewById(R.id.login_form);
+//        View mLoginFormView = findViewById(R.id.login_form);
+//        View mProgressView = findViewById(R.id.login_progress);
     }
 
     /**
